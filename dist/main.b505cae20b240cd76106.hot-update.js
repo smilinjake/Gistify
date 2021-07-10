b63a5c1a1f20eb03a5c71cc90f04fe7ffe99631f
Jacob Ward <smilinjake@gmail.com>
2021-07-10 14:18:31 -0500

    fixed bug issues

---
+++ dist/main.b505cae20b240cd76106.hot-update.js
@@ -0,0 +1,31 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdateGistify"]("main",{
+
+/***/ "./config.js":
+/*!*******************!*\
+  !*** ./config.js ***!
+  \*******************/
+/***/ ((module) => {
+
+eval("module.exports = {\n  TOKEN: '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 '\n};\n\n//# sourceURL=webpack://Gistify/./config.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ "use strict";
+/******/ 
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("0e34cf6948376a43b68e")
+/******/ })();
+/******/ 
+/******/ }
+);
